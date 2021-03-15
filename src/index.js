2db48c01bb5759b4b9fec1d9471481411b13f4bb
alehhunko <alehhunko@gmail.com>
2021-03-15 13:34:42 +0300

    fix: towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if( !Array.isArray(matrix) ){
+    return [];}
+  else { 
+    for (let i=0; i<matrix.length; i++){
+      if(i%2===1) {        
+      let t=matrix[i].reverse()
+      matrix.splice(i, 1, t);}
+    }
+  }
+  return matrix.flat();
 }
